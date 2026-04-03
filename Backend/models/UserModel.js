a4c318168f0e1520b7c8aa79de682a2dda72357a
dasarimaduri23 <dasarimaduri23@gmail.com>
2026-04-03 11:02:11 +0530

    Add user model schema

---
+++ Backend/models/UserModel.js
@@ -0,0 +1,48 @@
+import mongoose from "mongoose";
+
+// Define schema
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        match: [/^\S+@\S+\.\S+$/, 'Please enter a valid email address'],
+    },
+    password: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    avatar: {
+        type: String,
+        trim: true,
+        default: 'https://placehold.co/80x80.png?text=Profile', 
+    },
+    likedVideos: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Video',
+    }],
+    subscribedChannels: [{ 
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Channel',
+    }],
+    channel: { 
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Channel',
+        default: null,
+    },
+}, { timestamps: true });
+
+
+
+// Create model
+const UserModel = mongoose.models.User || mongoose.model('User', userSchema);
+
+// Export model
+export default UserModel;
